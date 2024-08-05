d5085085f7d154fd754d16c93f3f7e73b0dace87
syamkarthik0 <113086856+syamkarthik0@users.noreply.github.com>
2024-08-05 17:47:15 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import ResponsiveAppBar from './Appbar';
+function App(button) {
+  return (
+    <div className="App">
+      <ResponsiveAppBar/>
+    </div>
+    
+  );
+}
+
+export default App();
